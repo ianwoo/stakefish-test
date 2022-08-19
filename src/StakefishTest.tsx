e13c31c8a1650ed10f0dbfc0e29f8dd3f1f53e3a
0xAngel <0xangel@waterfalldefi.org>
2022-08-19 14:19:09 +0800

    console log the exchanges data

--- src/StakefishTest.tsx
+++ src/StakefishTest.tsx
@@ -1,6 +1,10 @@
 import "./global.scss";
+import { useCoingeckoExchangesData } from "./hooks/useCoingeckoExchangesData";
 
 function StakefishTest() {
+  const coingeckoExchangesData = useCoingeckoExchangesData();
+
+  console.log(coingeckoExchangesData);
   return (
     <div className="stakefish-test">
       <h1>Stakefish Test, Ian Woo</h1>
