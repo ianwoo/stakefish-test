7d15842570d0c2bfcbad225896309e7505725204
0xAngel <0xangel@waterfalldefi.org>
2022-08-19 14:33:36 +0800

    render exchanges

--- src/StakefishTest.tsx
+++ src/StakefishTest.tsx
@@ -1,13 +1,43 @@
+import TableRow from "./components/TableRow";
 import "./global.scss";
-import { useCoingeckoExchangesData } from "./hooks/useCoingeckoExchangesData";
+import { Exchange, useCoingeckoExchangesData } from "./hooks/useCoingeckoExchangesData";
 
 function StakefishTest() {
-  const coingeckoExchangesData = useCoingeckoExchangesData();
+  const coingeckoExchangesData: Exchange[] = useCoingeckoExchangesData();
 
   console.log(coingeckoExchangesData);
   return (
     <div className="stakefish-test">
       <h1>Stakefish Test, Ian Woo</h1>
+      <div className="header-row">
+        <div className="header first">
+          <span>Exchange Name</span>
+        </div>
+        <div className="header">
+          <span>Country</span>
+        </div>
+        <div className="header">
+          <span>URL</span>
+        </div>
+        <div className="header">
+          <span>Logo</span>
+        </div>
+        <div className="header last">
+          <span>Trust Rank</span>
+        </div>
+      </div>
+      {coingeckoExchangesData.map((ex: any, i: number) => (
+        <TableRow
+          key={i}
+          data={{
+            name: ex.name,
+            country: ex.country,
+            url: ex.url,
+            logo: ex.image,
+            trust_score_rank: ex.trust_score_rank,
+          }}
+        />
+      ))}
     </div>
   );
 }
--- src/hooks/useCoingeckoExchangesData.ts
+++ src/hooks/useCoingeckoExchangesData.ts
@@ -1,6 +1,21 @@
 import ky from "ky";
 import { useEffect, useState } from "react";
 
+export type Exchange = {
+  country: string;
+  description: string;
+  has_trading_incentive: boolean;
+  id: string;
+  image: string;
+  name: string;
+  trade_volume_24h_btc: number;
+  trade_volume_24h_btc_normalized: number;
+  trust_score: number;
+  trust_score_rank: number;
+  url: string;
+  year_established: number;
+}
+
 const getCoingeckoExchangesData = async () => {
   const result = await ky
     .get(
@@ -12,7 +27,7 @@ const getCoingeckoExchangesData = async () => {
 };
 
 export const useCoingeckoExchangesData = () => {
-  const [exchanges, setExchanges] = useState<any>({});
+  const [exchanges, setExchanges] = useState<Exchange[]>([]);
 
   useEffect(() => {
     const fetchBalance = async () => {
